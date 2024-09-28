d92f90d5e81629994ae2e7009171d706ae0b2bab
micablee <santiagomica711@gmail.com>
2024-09-28 21:00:30 +0800

    Create BProject.js

---
+++ BProject.js
@@ -0,0 +1,10 @@
+document.getElementById("item").style.backgroundColor = "#904e68";
+document.getElementById("item2").style.backgroundColor = "#904e68";
+document.getElementById("item3").style.backgroundColor = "#904e68";
+document.getElementById("item4").style.backgroundColor = "#904e68";
+document.getElementById("item5").style.backgroundColor = "#904e68";
+document.getElementById("item6").style.backgroundColor = "#904e68";
+document.getElementById("item7").style.backgroundColor = "#904e68";
+document.getElementById("item8").style.backgroundColor = "#904e68";
+document.getElementById("item9").style.backgroundColor = "#904e68";
+
